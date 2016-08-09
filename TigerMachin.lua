790bee9113461b2f1fc68e4dabe26b8a4b256e45
zcc <wade@wadedeMac-mini.local>
2016-08-09 11:27:48 +0800

    tm

--- TigerMachin.lua
+++ TigerMachin.lua
@@ -1,16 +1,4 @@
 local TigerMachinList = require "TigerMachinList1"
-local TigerMachin = {}
-
-TigerMachin.listview = {}
-
-function TigerMachin:init(listview)
-	self.listview = listview
-end
-
-function TigerMachin:addgood()
-	
-end
-
 
 
 
